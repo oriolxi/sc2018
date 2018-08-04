60ac3d58c8c868c30e6e0e1fbe94abe678d088bf
oriolxi <42074411+oriolxi@users.noreply.github.com>
2018-08-04 11:49:07 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#Cd to views folder (where html page is stored)
+cd /home/deployer/apps/cau/current/app/views
+
+#Remove old pages folder
+rm -rf pages
+
+#Clone git hub with new information
+git clone https://github.com/oriolxi/pages
+
+#Remove old sc2018 folder
+rm -rf /home/deployer/apps/cau/current/public/sc2018
+
+#Copy new sc2018 directory
+cp -r pages/sc2018 /home/deployer/apps/cau/current/public/sc2018
+
+#We finally reboot the system to make changes online
+reboot
